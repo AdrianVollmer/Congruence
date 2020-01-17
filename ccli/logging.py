1092bc57a8faf20038e9b3cd1a982f008123ec4d
Adrian Vollmer <adrian.vollmer@syss.de>
2020-01-17 23:21:49 +0100

    add empty files for logging, sql, keys

---
+++ ccli/logging.py
@@ -0,0 +1,34 @@
+#  ccli: A command line interface to Confluence
+#  Copyright (C) 2020  Adrian Vollmer
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import sys
+
+
+FORMAT = (
+    '%(levelname).1s %(asctime)-15s '
+    + '%(filename)s:%(lineno)d %(message)s'
+)
+
+
+logging.basicConfig(
+    stream=sys.stdout,
+    #  level=logging.DEBUG if args.DEBUG else logging.INFO,
+    format=FORMAT,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+log = logging.getLogger(__name__)
---
+++ ccli/sql.py
@@ -0,0 +1,23 @@
+#  ccli: A command line interface to Confluence
+#  Copyright (C) 2020  Adrian Vollmer
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from sqlalchemy import create_engine
+import xdg
+
+DB_FILE = os.path.join(xdg.XDG_DATA_HOME, "ccli", "db.sqlite")
+engine = create_engine('sqlite:///' + DB_FILE)
