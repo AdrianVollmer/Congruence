170fe27b72628be135467fb27777d0dcf8fbb567
Adrian Vollmer <adrian.vollmer@syss.de>
2020-02-23 10:19:33 +0100

    remove some duplicate code: open_content_in_cli_browser

--- congruence/confluence.py
+++ congruence/confluence.py
@@ -146,14 +146,7 @@ class CommentContextView(CongruenceTreeListBox):
     def ka_cli_browser(self, size=None):
         obj = self.focus.get_value()
         id = obj.id
-        log.debug("Build HTML view for page with id '%s'" % id)
-        rest_url = f"rest/api/content/{id}?expand=body.view"
-        r = make_request(rest_url)
-        content = r.json()
-        content = content['body']['view']['value']
-
-        content = f'<html><head></head><body>{content}</body></html>'
-        open_doc_in_cli_browser(content.encode(), self.app)
+        open_content_in_cli_browser(self.app, id)
 
     def ka_gui_browser(self, size=None):
         obj = self.focus.get_value()
@@ -252,14 +245,7 @@ class PageView(CongruenceTextBox):
 
     def ka_cli_browser(self, size=None):
         id = self.obj.id
-        log.debug("Build HTML view for page with id '%s'" % id)
-        rest_url = f"rest/api/content/{id}?expand=body.view"
-        r = make_request(rest_url)
-        content = r.json()
-        content = content["body"]["view"]["value"]
-
-        content = f"<html><head></head><body>{content}</body></html>"
-        open_doc_in_cli_browser(content.encode(), self.app)
+        open_content_in_cli_browser(self.app, id)
 
     def ka_gui_browser(self, size=None):
         id = self.obj.id
@@ -396,14 +382,7 @@ class ContentList(CongruenceListBox):
     def ka_cli_browser(self, size=None):
         node = self.get_focus()[0]
         id = node.obj.id
-        log.debug("Build HTML view for page with id '%s'" % id)
-        rest_url = f"rest/api/content/{id}?expand=body.view"
-        r = make_request(rest_url)
-        content = r.json()
-        content = content["body"]["view"]["value"]
-
-        content = f"<html><head></head><body>{content}</body></html>"
-        open_doc_in_cli_browser(content.encode(), self.app)
+        open_content_in_cli_browser(self.app, id)
 
     def ka_gui_browser(self, size=None):
         node = self.get_focus()[0]
@@ -427,3 +406,14 @@ class ContentList(CongruenceListBox):
             self.params["start"] += \
                 self.params["limit"]
         return result
+
+
+def open_content_in_cli_browser(app, id):
+    log.debug("Build HTML view for page with id '%s'" % id)
+    rest_url = f"rest/api/content/{id}?expand=body.view"
+    r = make_request(rest_url)
+    content = r.json()
+    content = content["body"]["view"]["value"]
+
+    content = f"<html><head></head><body>{content}</body></html>"
+    open_doc_in_cli_browser(content.encode(), app)
